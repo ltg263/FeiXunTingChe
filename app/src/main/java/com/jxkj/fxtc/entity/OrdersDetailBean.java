2ee329b4165de74591811d156c1ccf4f6270f4e4
qianzhanjie <qzj842179561@gmail.com>
2021-01-07 18:51:46 +0800

    ui

---
+++ app/src/main/java/com/jxkj/fxtc/entity/OrdersDetailBean.java
@@ -0,0 +1,168 @@
+package com.jxkj.fxtc.entity;
+
+public class OrdersDetailBean {
+
+    /**
+     * address : 宁波海曙
+     * creatTime : 2020-12-25 17:10:43
+     * delTF : 0
+     * endTime : 2020-12-25 17:10:41
+     * id : 16
+     * license : 浙·T7797
+     * lotID : 1
+     * orderNo : TNO20122517100121
+     * orderPrice : 1340
+     * orderType : 0
+     * seatID : 2
+     * seatName : D2
+     * startTime : 2020-12-25 16:02:55
+     * status : 0
+     * useTime : 1
+     * userId : 12
+     */
+
+    private String address;
+    private String creatTime;
+    private String delTF;
+    private String endTime;
+    private String id;
+    private String license;
+    private String lotID;
+    private String orderNo;
+    private String orderPrice;
+    private String orderType;
+    private String seatID;
+    private String seatName;
+    private String startTime;
+    private String status;
+    private String useTime;
+    private String userId;
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCreatTime() {
+        return creatTime;
+    }
+
+    public void setCreatTime(String creatTime) {
+        this.creatTime = creatTime;
+    }
+
+    public String getDelTF() {
+        return delTF;
+    }
+
+    public void setDelTF(String delTF) {
+        this.delTF = delTF;
+    }
+
+    public String getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(String endTime) {
+        this.endTime = endTime;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getLicense() {
+        return license;
+    }
+
+    public void setLicense(String license) {
+        this.license = license;
+    }
+
+    public String getLotID() {
+        return lotID;
+    }
+
+    public void setLotID(String lotID) {
+        this.lotID = lotID;
+    }
+
+    public String getOrderNo() {
+        return orderNo;
+    }
+
+    public void setOrderNo(String orderNo) {
+        this.orderNo = orderNo;
+    }
+
+    public String getOrderPrice() {
+        return orderPrice;
+    }
+
+    public void setOrderPrice(String orderPrice) {
+        this.orderPrice = orderPrice;
+    }
+
+    public String getOrderType() {
+        return orderType;
+    }
+
+    public void setOrderType(String orderType) {
+        this.orderType = orderType;
+    }
+
+    public String getSeatID() {
+        return seatID;
+    }
+
+    public void setSeatID(String seatID) {
+        this.seatID = seatID;
+    }
+
+    public String getSeatName() {
+        return seatName;
+    }
+
+    public void setSeatName(String seatName) {
+        this.seatName = seatName;
+    }
+
+    public String getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(String startTime) {
+        this.startTime = startTime;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getUseTime() {
+        return useTime;
+    }
+
+    public void setUseTime(String useTime) {
+        this.useTime = useTime;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+}
