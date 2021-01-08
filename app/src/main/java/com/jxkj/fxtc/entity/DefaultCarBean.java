ddd4ff7d16233d9ed8f9eee9c21edb1f490f1291
qianzhanjie <qzj842179561@gmail.com>
2021-01-08 19:24:09 +0800

    ui

---
+++ app/src/main/java/com/jxkj/fxtc/entity/DefaultCarBean.java
@@ -0,0 +1,98 @@
+package com.jxkj.fxtc.entity;
+
+public class DefaultCarBean {
+
+    /**
+     * creatime : 2020-12-24 11:20:51
+     * defaultCar : 1
+     * delTF : 0
+     * id : 3
+     * license : 浙·T7797
+     * status : 1
+     * type : 0
+     * updateTime : 2020-12-24 11:20:58
+     * userID : 12
+     */
+
+    private String creatime;
+    private String defaultCar;
+    private String delTF;
+    private String id;
+    private String license;
+    private String status;
+    private String type;
+    private String updateTime;
+    private String userID;
+
+    public String getCreatime() {
+        return creatime;
+    }
+
+    public void setCreatime(String creatime) {
+        this.creatime = creatime;
+    }
+
+    public String getDefaultCar() {
+        return defaultCar;
+    }
+
+    public void setDefaultCar(String defaultCar) {
+        this.defaultCar = defaultCar;
+    }
+
+    public String getDelTF() {
+        return delTF;
+    }
+
+    public void setDelTF(String delTF) {
+        this.delTF = delTF;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getLicense() {
+        return license;
+    }
+
+    public void setLicense(String license) {
+        this.license = license;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(String updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    public String getUserID() {
+        return userID;
+    }
+
+    public void setUserID(String userID) {
+        this.userID = userID;
+    }
+}
