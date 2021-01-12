6cefc4822e9b66c3d6280104ab05774c99eb47f1
qianzhanjie <qzj842179561@gmail.com>
2021-01-12 17:42:50 +0800

    ui

--- app/src/main/java/com/jxkj/fxtc/view/adapter/BookingSpaceAdapter.java
+++ app/src/main/java/com/jxkj/fxtc/view/adapter/BookingSpaceAdapter.java
@@ -32,14 +32,23 @@ public class BookingSpaceAdapter extends BaseQuickAdapter<LotListBean.ListBean, 
                 .setText(R.id.tv_jl, "距你" + item.getDistance() + "km")
                 .setText(R.id.tv_cw, "剩余车位" + item.getSeatCount() + "个")
                 .setVisible(R.id.tv_yycp,false)
+                .setVisible(R.id.iv1,false)
+                .setVisible(R.id.iv2,false)
+                .setVisible(R.id.iv3,false)
                 .setVisible(R.id.tv_dh,false);
 
         if(mContext instanceof BookingSpaceActivity){
-            helper.setVisible(R.id.tv_yycp,true);
             helper.setBackgroundRes(R.id.tv_yycp,R.drawable.btn_shape_theme);
             if(Integer.valueOf(item.getSeatCount())==0){
                 helper.setText(R.id.tv_time, "--/小时").setText(R.id.tv_cw, "剩余车位--个");
                 helper.setBackgroundRes(R.id.tv_yycp,R.drawable.btn_shape_d8d8);
+            }
+
+            if(item.getIsRecommend().equals("1")){
+                helper.setVisible(R.id.iv1,true)
+                        .setVisible(R.id.iv2,true)
+                        .setVisible(R.id.iv3,true)
+                        .setVisible(R.id.tv_yycp,true);
             }
         }else{
             helper.setVisible(R.id.tv_dh,true);
